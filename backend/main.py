1294d22b6178a61d4b916062f1286766dad12669
VishnuGowdaHC <vishnugowdahc2005@gmail.com>
2026-05-13 17:35:26 +0530

    bugfix#2

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://heart-disease-prediction-1-9u7r.onrender.com/"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://heart-disease-prediction-1-9u7r.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
